a43e3ae818c46d6d6e34ae31001ce0c9f19dd722
ay0ks <43708417+ay0ks@users.noreply.github.com>
2023-09-25 23:08:17 +0200

    feat: parser

--- OAP.c
+++ OAP.c
@@ -494,16 +494,3 @@ void OAP_parse(OAP *parser, int argument_count, char **argument_vector) {
     else if (OAP_is(OAP_getopt(parser, "version"), "true"))
         OAP_version(parser);
 }
-
-int main(int argc, char **argv) {
-    OAP *oap = OL_new(OAP, "pg", "10000", NULL);
-    OAP_addopt(oap, "Enable verbose output.", NULL, NULL, NULL, "verbose", false, false, false, 3, "Va", "Verbose", "Vjblinkwjsus");
-    OAP_addarg(oap, "FILE", "jjdjsjs", "/a.txt", "fd", false);
-
-    OAP_addopt(oap, "Enable verbose 2.", "STRING", "Your name", "Daniel", "jj", true, false, false, 3, "Ab", "Abce", "Abcerum");
-    OAP_parse(oap, argc, argv);
-    OL_del(OAP, oap);
-    
-    printf("ab: %s\n", OAP_getopt(oap, "jj"));
-    
-}
